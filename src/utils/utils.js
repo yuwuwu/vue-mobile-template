03a6829535bb3ca4ecea5abfda2ca3e389eb8bcd
yuyongxing@pxene.com <yuyongxing@pxene.com>
2021-10-18 18:11:53 +0800

    添加工具类

---
+++ src/utils/utils.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: yuyongxing
+ * @Date: 2021-10-18 18:08:24
+ * @LastEditors: yuyongxing
+ * @LastEditTime: 2021-10-18 18:10:27
+ * @Description:util
+ */
+import { Toast } from "mint-ui"
+export const toast = (text, options) => {
+  Toast(text)
+}
