7c40563555d989f334f42448583072537474c8ab
yuyongxing@pxene.com <yuyongxing@pxene.com>
2021-10-18 18:07:20 +0800

    引入mintUi

--- src/main.js
+++ src/main.js
@@ -2,17 +2,19 @@
  * @Author: yuyongxing
  * @Date: 2021-10-18 11:40:35
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-10-18 15:39:51
+ * @LastEditTime: 2021-10-18 18:03:25
  * @Description:
  */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Toast } from 'mint-ui'
+
+import Mint from 'mint-ui';
+Vue.use(Mint);
+import '../node_modules/mint-ui/lib/style.css'
 
 Vue.config.productionTip = false
-Vue.use(Toast)
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,33 +2,49 @@
  * @Author: yuyongxing
  * @Date: 2021-10-18 11:40:35
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-10-18 15:24:22
+ * @LastEditTime: 2021-10-18 18:05:57
  * @Description:
 -->
 <template>
   <div class="home">
     <img src="../assets/logo.png">
     <HelloWorld msg="hello vue-mobile-template" />
+    <button @click="showToast" />
+    <mt-button type="default">
+      default
+    </mt-button>
+    <mt-button type="primary">
+      primary
+    </mt-button>
+    <mt-button type="danger">
+      danger
+    </mt-button>
   </div>
 </template>
 
 <script>
+import { Toast } from "mint-ui"
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    showToast() {
+      Toast("13123")
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.home{
+.home {
   width: 100%;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  img{
+  img {
     width: 100px;
   }
 }
