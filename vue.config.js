571b9a22f0ae4feed12ed319fcc8bb8930f646fd
yuyongxing@pxene.com <yuyongxing@pxene.com>
2021-10-19 17:21:51 +0800

    添加环境配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,25 @@
+
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  publicPath: './',
+  assetsDir: './',
+  // webpack-dev-server 相关配置
+  devServer: {
+    host: '0.0.0.0',
+    port: 80,
+    open: true,
+    proxy: {
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: ``,
+        changeOrigin: true,
+        ws: true,
+        secure: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: '/stage-api'
+        }
+      }
+    },
+    disableHostCheck: true
+  }
 }
