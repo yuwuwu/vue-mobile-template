df4f96691d76ed14a92e9cc34d90d1b56aac8048
yuyongxing@pxene.com <yuyongxing@pxene.com>
2021-10-25 17:40:21 +0800

    设置页面标题为当前路由设置的title

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: yuyongxing
  * @Date: 2021-10-18 11:40:35
  * @LastEditors: yuyongxing
- * @LastEditTime: 2021-10-19 14:27:07
+ * @LastEditTime: 2021-10-25 17:39:51
  * @Description:
  */
 import Vue from 'vue'
@@ -16,12 +16,13 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    meta: { keepAlive: true },
+    meta: { title: "主页", keepAlive: true },
     component: Home
   },
   {
     path: '/login',
     name: 'login',
+    meta: { title: "登录", keepAlive: true },
     component: Login
   }
 ]
@@ -29,5 +30,9 @@ const routes = [
 const router = new VueRouter({
   routes
 })
-
+// 路由守卫，修改页面标题为路由mate设置的值
+router.beforeEach((to, from, next) => {
+  document.title = to?.meta?.title ?? 'vue-mobile-template'
+  next()
+})
 export default router
--- vue.config.js
+++ vue.config.js
@@ -3,31 +3,24 @@ module.exports = {
   productionSourceMap: false,
   publicPath: './',
   assetsDir: './',
-  chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].title = 'vue-mobile-template'
-        return args
-      })
-  },
   // webpack-dev-server 相关配置
   devServer: {
     host: '0.0.0.0',
     port: 80,
     open: true,
-    proxy: {
-      // detail: https://cli.vuejs.org/config/#devserver-proxy
-      [process.env.VUE_APP_BASE_API]: {
-        target: ``,
-        changeOrigin: true,
-        ws: true,
-        secure: true,
-        pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: '/stage-api'
-        }
-      }
-    },
+    proxy: null,
+    // proxy: {
+    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
+    //   [process.env.VUE_APP_BASE_API]: {
+    //     target: ``,
+    //     changeOrigin: true,
+    //     ws: true,
+    //     secure: true,
+    //     pathRewrite: {
+    //       ['^' + process.env.VUE_APP_BASE_API]: '/stage-api'
+    //     }
+    //   }
+    // },
     disableHostCheck: true
   }
 }
